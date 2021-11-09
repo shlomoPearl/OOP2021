cbd6866524b25cfad5b0e99ba8d8b8475affd09f
Hp <dovi.amiram.91@gmail.com>
2021-11-09 18:26:10 +0200

    first commit

---
+++ Elevator.py
@@ -0,0 +1,10 @@
+class Elevator:
+    def __init__(self, elevator_id, speed, min_floor, max_floor, close_time, open_time, start_time, stop_time):
+        self.id = elevator_id
+        self.speed = speed
+        self.min_floor = min_floor
+        self.max_floor = max_floor
+        self.close_time = close_time
+        self.open_time = open_time
+        self.start_time = start_time
+        self.stop_time = stop_time

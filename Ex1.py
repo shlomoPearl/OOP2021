1a5320915559cff3f478d3daf3229463486b9f35
Hp <dovi.amiram.91@gmail.com>
2021-11-09 21:36:34 +0200

    test

--- Ex1.py
+++ Ex1.py
@@ -1,7 +1,10 @@
 import csv
 import json
+import sys
 
 from pprint import pprint
+from random import randint
+
 from Building import Building
 from Elevator import Elevator
 from ElevatorCall import ElevatorCall
@@ -13,7 +16,8 @@ def create_building_from_file(file_name):
         file.close()
     elevators = create_elevator_list_from_json(json_building["_elevators"])
     new_building = Building(min_floor=json_building["_minFloor"],
-                            max_floor=json_building["_maxFloor"], elevators=elevators)
+                            max_floor=json_building["_maxFloor"],
+                            elevators=elevators)
     return new_building
 
 
@@ -46,10 +50,10 @@ def create_list_of_call_from_csv(file_name):
 
     for call in csv_call_list:
         new_call = ElevatorCall(
-            time_of_call=call[call_parameters["time"]],
-            source=call[call_parameters["source"]],
-            destination=call[call_parameters["destination"]],
-            allocated_to_elevator=call[call_parameters["allocated_to_elevator"]]
+            time_of_call=float(call[call_parameters["time"]]),
+            source=int(call[call_parameters["source"]]),
+            destination=int(call[call_parameters["destination"]]),
+            allocated_to_elevator=int(call[call_parameters["allocated_to_elevator"]])
         )
         call_list.append(new_call)
     return call_list
@@ -59,24 +63,34 @@ def write_to_csv(list_of_calls, file_name):
     e = "Elevator call"
     csv_list_of_calls = []
     for call in list_of_calls:
-        csv_list_of_calls.append([e,
-                                  str(call.time_of_call),
-                                  str(call.source),
-                                  str(call.destination),
-                                  str('0'),
-                                  str(call.allocated_to_elevator)])
-    with open(file_name, 'w', newline='') as file:
+        string_list = [
+            e,
+            str(call.time_of_call),
+            str(call.source),
+            str(call.destination),
+            '0',
+            str(call.allocated_to_elevator)]
+        csv_list_of_calls.append(string_list)
+    with open(file_name, 'w') as file:
         csv_writer = csv.writer(file)
-        csv_writer.writerow(csv_list_of_calls)
+        csv_writer.writerows(csv_list_of_calls)
 
 
-def offline_algorithm(json_building_file_name, csv_list_of_calls_file_name, csv_output_file_name):
+def offline_algorithm(json_building_file_name, csv_input_file_name, csv_output_file_name):
     building = create_building_from_file(json_building_file_name)
-    list_of_calls = create_list_of_call_from_csv(csv_list_of_calls_file_name)
+    list_of_calls = create_list_of_call_from_csv(csv_input_file_name)
     for call in list_of_calls:
-        call.allocated_to_elevator = 0
+        call.allocated_to_elevator = randint(1, 5)
     write_to_csv(list_of_calls, csv_output_file_name)
 
 
-print("3")
+def main():
+    files = {"json_building_file_name": 0, "csv_input_file_name": 1, "csv_output_file_name": 2}
+    file_names = sys.argv[1:]
+    offline_algorithm(file_names[files["json_building_file_name"]],
+                      file_names[files["csv_input_file_name"]],
+                      file_names[files["csv_output_file_name"]])
 
+
+if __name__ == '__main__':
+    main()
